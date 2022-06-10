1ec4bab4863418cbc149d81e042f339c168f0b47
baijeehan <jaomar@usm.edu.ph>
2022-06-10 21:23:31 +0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://omar-bookstore.netlify.app';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://jeehanomar-001-site1.dtempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false
+  },
+  apis: {
+    default: {
+      url: 'http://jeehanomar-001-site1.dtempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
